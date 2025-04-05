7b188863f162e44f4a2b70d8b1118e63d8236129
MahdiHDev <mahdi.hussain.work@gmail.com>
2025-04-05 23:50:59 +0600

    adding login functionality using axios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,68 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Card from '../components/Card';
 
 const Login = () => {
+    const [formValue, setFormValue] = useState({
+        emailOrMobile: '',
+        password: '',
+    });
+
+    const handleInput = (e) => {
+        setFormValue({
+            ...formValue,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(
+                'http://localhost:3000/auth/login',
+                formValue
+            );
+            console.log(response.data);
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+
     return (
         <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
             <Card
                 className="w-screen md:w-96 bg-gray-100 md:bg-white"
                 title="Login"
             >
-                <form action="" className="space-y-4 flex flex-col text-sm">
+                <form
+                    action=""
+                    className="space-y-4 flex flex-col text-sm"
+                    onSubmit={handleSubmit}
+                >
                     <input
                         type="text"
-                        name="email"
+                        value={formValue.emailOrMobile}
+                        onChange={handleInput}
+                        name="emailOrMobile"
                         className="border p-2 rounded-sm border-gray-400  outline-gray-500"
                         placeholder="Enter Email or Phone Number"
                     />
                     <input
                         type="password"
+                        value={formValue.password}
+                        onChange={handleInput}
                         name="password"
                         className="border p-2 rounded-sm border-gray-400  outline-gray-500"
                         placeholder="Enter Password"
                     />
+                    <button
+                        type="submit"
+                        className="w-full bg-gray-800 text-white p-2 rounded-sm"
+                    >
+                        Submit
+                    </button>
                 </form>
-                <button
-                    type="submit"
-                    className="w-full bg-gray-800 text-white p-2 rounded-sm mt-4"
-                >
-                    Submit
-                </button>
 
                 <div className="text-sm py-2">
                     Don&apos;t have an account.{' '}
